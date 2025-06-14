649acf0cc1a9a8edefa2b6edc70fdf3b30b04b66
vanha2301 <vanha.cv.vn@gmail.com>
2025-06-14 23:39:35 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <SD.h>
+
+#define SD_CS 5
+
+void setup() {
+  Serial.begin(112500);
+  
+  // create SD card
+  if (!SD.begin(SD_CS)) {
+    Serial.println("Không thể khởi động thẻ SD!");
+    return;
+  }
+  Serial.println("Đã kết nối thẻ SD thành công.");
+
+  File file = SD.open("/test.txt");
+  if (file) {
+    Serial.println("Đang đọc file test.txt:");
+    while (file.available()) {
+      Serial.write(file.read());
+    }
+    file.close();
+  } else {
+    Serial.println("Không mở được file.");
+  }
+
+}
+
+void loop() {}
