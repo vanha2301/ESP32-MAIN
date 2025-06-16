4ca03e8e281eeec24e49be8628340625fce4b06d
vanha2301 <vanha.cv.vn@gmail.com>
2025-06-17 02:48:47 +0700

    check

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
 #include <SPI.h>
 #include <SD.h>
+#include <WiFi.h>
 
 #define SD_CS 5
+
+// define wifi ssid and password
+const char* ssid = "vanha2301";
+const char* password = "123456789";
 
 void setup() {
   Serial.begin(112500);
   
+  // connect wifi 
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // connect wifi end.
+
+
   // create SD card
   if (!SD.begin(SD_CS)) {
     Serial.println("Không thể khởi động thẻ SD!");
